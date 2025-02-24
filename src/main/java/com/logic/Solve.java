a9d93468da016808f3a9c247555c71c419168007
Stahlynx <168390257+TheoK13523154@users.noreply.github.com>
2025-02-24 12:28:18 +0700

    final touch

--- src/main/java/com/logic/Solve.java
+++ src/main/java/com/logic/Solve.java
@@ -9,42 +9,44 @@ import javafx.application.Platform;
 
 public class Solve {
     public static int moveCount = 0;
-    
+    public static boolean isSolved = false; 
+
     public static boolean Solve(char[][] board, List<char[][]> pieces, int index, MainController controller) {
         if (isComplete(board)) return true;
-    
-        char[][] piece = pieces.get(index);
-        char pieceLabel = (char) ('A' + index);
-    
-        List<char[][]> orientations = getAllOrientations(piece);
-    
-        for (char[][] orientation : orientations) { 
-            for (int row = 0; row < board.length; row++) {
-                for (int col = 0; col < board[0].length; col++) {
-                    if (canPlacePiece(board, orientation, row, col)) {
-                        placePiece(board, orientation, row, col, pieceLabel);
-                        moveCount++;
-    
-                        Platform.runLater(() -> controller.updateMoveCount(moveCount, 0));
-    
-                        clearScreen();
-                        MatrixUtils.printMatrix(board);
-    
-                        Platform.runLater(() -> controller.updateBoard(board));
-    
-                        if (Solve(board, pieces, index + 1, controller)) return true;
-    
-                        removePiece(board, orientation, row, col);
-                        clearScreen();
-                        MatrixUtils.printMatrix(board);
-                        Platform.runLater(() -> controller.updateBoard(board));
+        else{
+            char[][] piece = pieces.get(index);
+            char pieceLabel = (char) ('A' + index);
+        
+            List<char[][]> orientations = getAllOrientations(piece);
+        
+            for (char[][] orientation : orientations) { 
+                for (int row = 0; row < board.length; row++) {
+                    for (int col = 0; col < board[0].length; col++) {
+                        if (canPlacePiece(board, orientation, row, col)) {
+                            placePiece(board, orientation, row, col, pieceLabel);
+                            moveCount++;
+        
+                            Platform.runLater(() -> controller.updateMoveCount(moveCount, 0));
+        
+                            // clearScreen();
+                            // MatrixUtils.printMatrix(board);
+        
+                            Platform.runLater(() -> controller.updateBoard(board));
+        
+                            if (Solve(board, pieces, index + 1, controller)) return true;
+        
+                            removePiece(board, orientation, row, col);
+                            // clearScreen();
+                            // MatrixUtils.printMatrix(board);
+                            Platform.runLater(() -> controller.updateBoard(board));
+                        }
                     }
                 }
             }
+            return false;
         }
-        return false;
     }
-    
+
     public static void moveMatrix(List<int[][]> L1, List<int[][]> L2) {
         if (!L1.isEmpty()) {
             int[][] matrix = L1.remove(0); 
@@ -52,45 +54,42 @@ public class Solve {
         }
     }
   
-  public static List<char[][]> getAllOrientations(char[][] piece) {
-    List<char[][]> orientations = new ArrayList<>();
+    public static List<char[][]> getAllOrientations(char[][] piece) {
+        List<char[][]> orientations = new ArrayList<>();
     
-    char[][] rotated = piece;
-    orientations.add(rotated);
-    for (int i = 0; i < 3; i++) {  
-        rotated = MatrixUtils.rotateMatrix(rotated);
-        orientations.add(rotated);
+        orientations.add(piece);
+        Arrays.stream(new int[]{0, 1, 2, 3}).forEach(i -> {
+            char[][] rotated = MatrixUtils.rotateMatrix(orientations.get(orientations.size() - 1));
+            orientations.add(rotated);
+        });
+    
+        char[][] flipped = MatrixUtils.mirrorHorizontal(piece);
+        orientations.add(flipped);
+        Arrays.stream(new int[]{0, 1, 2, 3}).forEach(i -> {
+            char[][] rotated = MatrixUtils.rotateMatrix(orientations.get(orientations.size() - 1));
+            orientations.add(rotated);
+        });
+    
+        return orientations;
     }
     
-    char[][] flipped = MatrixUtils.mirrorHorizontal(piece);
-    rotated = flipped;
-    orientations.add(rotated);
-    for (int i = 0; i < 3; i++) {
-        rotated = MatrixUtils.rotateMatrix(rotated);
-        orientations.add(rotated);
-    }
-    return orientations;
-  }
 
-  public static boolean canPlacePiece(char[][] board, char[][] piece, int startRow, int startCol) {
-    for (int i = 0; i < piece.length; i++) {
-        for (int j = 0; j < piece[i].length; j++) {
-            if (piece[i][j] != '.') {
-                int boardRow = startRow + i;
-                int boardCol = startCol + j;
-                
-                if (boardRow >= board.length || boardCol >= board[0].length) {
-                    return false;
-                }
-                
-                if (board[boardRow][boardCol] != '.') {
-                    return false;
+    public static boolean canPlacePiece(char[][] board, char[][] piece, int startRow, int startCol) {
+        if (startRow + piece.length > board.length || startCol + piece[0].length > board[0].length) {
+            return false; 
+        }
+    
+        for (int i = 0; i < piece.length; i++) {
+            for (int j = 0; j < piece[i].length; j++) {
+                if (piece[i][j] != '.' && board[startRow + i][startCol + j] != '.') {
+                    return false; 
                 }
             }
         }
+    
+        return true;
     }
-    return true;
-  }
+    
 
   public static void removePiece(char[][] board, char[][] piece, int startRow, int startCol) {
     for (int i = 0; i < piece.length; i++) {
@@ -135,14 +134,14 @@ public class Solve {
         System.out.flush();
   }
 
-  public static boolean isComplete(char[][] board) {
-    for (char[] row : board) {
-        for (char cell : row) {
-            if (cell == '.') {
-                return false;                
-            }
-        }        
+    public static boolean isComplete(char[][] board) {
+        for (char[] row : board) {
+            for (char cell : row) {
+                if (cell == '.') {
+                    return false;                
+                }
+            }        
+        }
+        return true;
     }
-    return true;
-  }
 }
